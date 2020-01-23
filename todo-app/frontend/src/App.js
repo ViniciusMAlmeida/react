477895c8b0c2f551bc73deca3785795aedcfe2e8
ViniciusMAlmeida <vinicius.almeida@coopertec.com.br>
2020-01-23 17:09:53 -0300

    Componente App

---
+++ todo-app/frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import '../node_modules/@fortawesome/fontawesome-free/js/all.js'
+
+export default props => (
+  <div className='container'>
+    <h1><span className="far fa-angry"></span>Teste</h1>
+  </div>
+)
