2720eefdeb2461ec3fe4de7af8fc32bed424692d
ViniciusMAlmeida <vinicius.almeida@coopertec.com.br>
2020-01-23 18:03:12 -0300

    Componentes Todo e About

--- todo-app/frontend/src/App.js
+++ todo-app/frontend/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
 import '../node_modules/@fortawesome/fontawesome-free/js/all.js'
 
+import React from 'react';
+import Todo from './todo/todo'
+import About from './about/about'
+
 export default props => (
   <div className='container'>
-    <h1><span className="far fa-angry"></span>Teste</h1>
+    <Todo />
+    <About />
   </div>
 )
---
+++ todo-app/frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1><span className="far fa-angry"></span>Teste</h1>
+            </div>
+        )
+    }
+}
