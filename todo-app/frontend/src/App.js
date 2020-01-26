dec907f337814a4e79990b67a31855fe5578cf9a
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2020-01-26 14:19:57 -0300

    Componente Menu

--- todo-app/frontend/src/App.js
+++ todo-app/frontend/src/App.js
@@ -5,9 +5,11 @@ import '../node_modules/@fortawesome/fontawesome-free/js/all.js'
 import React from 'react';
 import Todo from './todo/todo'
 import About from './about/about'
+import Menu from './template/menu'
 
 export default props => (
   <div className='container'>
+    <Menu />
     <Todo />
     <About />
   </div>
