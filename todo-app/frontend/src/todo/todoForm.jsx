27b2836e9cc4472201226b91fa6bd897a8c94eee
ViniciusMAlmeida <vinicius.almeida@coopertec.com.br>
2020-01-31 14:13:57 -0300

    Melhorias de CSS e Limpar Formulário

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Grid from '../template/grid'
-import IconButton from '../template/iconButton'
+import React from "react";
+import Grid from "../template/grid";
+import IconButton from "../template/iconButton";
 
 export default props => (
-    <div role='form' className='todoForm form-row'>
-        <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'
-                value={props.description} onChange={props.handleChange}/>
-        </Grid>
-        <Grid cols='12 3 2'>
-            <IconButton styles='primary mr-2' icon='plus' onClick={props.handleAdd} />
-            <IconButton styles='info' icon='search' onClick={props.handleSearch} />
-        </Grid>   
-    </div>
-)
+  <div role="form" className="todoForm form-row mb-5">
+    <Grid cols="12 9 10">
+      <input
+        id="description"
+        className="form-control"
+        placeholder="Adicione uma tarefa"
+        value={props.description}
+        onChange={props.handleChange}
+      />
+    </Grid>
+    <Grid cols="12 3 2">
+      <IconButton styles="primary mr-2" icon="plus" onClick={props.handleAdd} />
+      <IconButton
+        styles="info mr-2"
+        icon="search"
+        onClick={props.handleSearch}
+      />
+      <IconButton styles="light" icon="times" onClick={props.handleClear} />
+    </Grid>
+  </div>
+);
--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -1,37 +1,46 @@
-import React from 'react'
-import IconButton from '../template/iconButton'
-import '../template/custom.css'
+import React from "react";
+import IconButton from "../template/iconButton";
+import "../template/custom.css";
 
 export default props => {
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td className={todo.done ? "markedAsDone" : ""}>{todo.description}</td>
+        <td>
+          <IconButton
+            styles="success mr-2"
+            icon="check"
+            hide={todo.done}
+            onClick={() => props.handleMarkAsDone(todo)}
+          />
+          <IconButton
+            styles="warning mr-2"
+            icon="undo"
+            hide={!todo.done}
+            onClick={() => props.handleMarkAsPending(todo)}
+          />
+          <IconButton
+            styles="danger mr-2"
+            icon="trash-alt"
+            hide={!todo.done}
+            onClick={() => props.handleRemove(todo)}
+          />
+        </td>
+      </tr>
+    ));
+  };
 
-    const renderRows = () => {
-        const list = props.list || []
-        return list.map(todo =>(
-            <tr key={todo._id}>
-                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
-                <td>
-                    <IconButton styles='success mr-2' icon='check' hide={todo.done}
-                        onClick={() => props.handleMarkAsDone(todo)} />
-                    <IconButton styles='warning mr-2' icon='undo' hide={!todo.done}
-                        onClick={() => props.handleMarkAsPending(todo)} />
-                    <IconButton styles='danger mr-2' icon='trash-alt' hide={!todo.done}
-                        onClick={() => props.handleRemove(todo)} />
-                </td>
-            </tr>
-        ))
-    }
-
-    return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Descrição</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                {renderRows()}
-            </tbody>
-        </table>
-    )
-}
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th className="tableActions">Ações</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows()}</tbody>
+    </table>
+  );
+};
